4aef9f3882b644947f188df7d1e46ec552e60494
Kauazinrrrrr <00001094332896sp@al.educacao.sp.gov.br>
2024-11-14 08:57:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Desenho',
+    'Qual desenho tem um passaro azul e vermelho?',
+    'Pica-Pau'
+)
+
+criaCartao(
+    'Idioma',
+    'Qual lingua os brasileiros falam?',
+    'Português'
+)
+
+criaCartao(
+    'Galáxia ',
+    'qual nossa galáxia?',
+    'Via láctea'
+)
+
+criaCartao(
+    'Continentes',
+    'Quantos continentes existem?',
+    '6'
+)
